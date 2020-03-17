f64d89bcb79904f7f9d4619b151de6e6d0824190
John <highbattery2003@gmail.com>
2020-03-17 22:34:29 +0800

    update get function

--- TestB/resources/api_search.py
+++ TestB/resources/api_search.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 from flask import request
 from TestB.tools.api_result import apiResult
 
-# from TestB.adapter.house_repository import search_house
+from TestB.adapter.house_repository import search_house
 class House_Search(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -21,21 +21,7 @@ class House_Search(Resource):
         try:
             arg = self.reqparse.parse_args()
             request
-            # data = search_house(arg)
-            data = {"rent_list":[[
-       {
-        "landlord_name": "吳小姐",
-"landlord_status":"屋主",
-"phone":"02-22223333",
-"building_type":"公寓",
-"house_type": "獨立套房",
-"sex_requirement": 1,
-"own_sex": 2,
-"landlord_first_name": "吳",
-"region_name": "台北市",
-"is_own": True
-      }
-  ]]}
+            data = search_house(arg)
             return apiResult(200, data, 'Get rent house success')
         except Exception as e:
             return apiResult(500, str(e), 'something wrong: ' + str(e))
